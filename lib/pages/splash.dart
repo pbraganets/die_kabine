fa83902010f488350812ebcea7af01bb60c4f4a6
Pavlo Braganets <pbraganets@gmail.com>
2020-04-05 15:19:06 +0300

    Implement Logo

--- lib/pages/splash.dart
+++ lib/pages/splash.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:diekabine/widgets/logo.dart';
 
 class Splash extends StatefulWidget {
 
@@ -11,7 +12,17 @@ class _SplashState extends State<Splash> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.lightBlueAccent,
+      color: Color(0xFF3C52A0),
+      child: Align(
+        alignment: Alignment.center,
+        child: FractionallySizedBox(
+            heightFactor: 1.00 / 6.00,
+            child: Align(
+                alignment: Alignment.center,
+                child: Logo()
+            )
+        ),
+      ),
     );
   }
 }
--- lib/widgets/logo.dart
+++ lib/widgets/logo.dart
@@ -1,11 +1,77 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class Logo extends StatelessWidget {
+  final containerWidth = 720.0;
+  final containerHeight = 283.0;
+  final polygonWidth = 274.54;
+  final polygonHeight = 283.0;
+  final titleY = 86.8;
+  final titleWidth = 633.0;
+  final titleHeight = 155.0;
+  final kX = 215.77;
+  final kY = 92.27;
+  final kWidth = 80.95;
+  final kHeight = 93.30;
+  final polygonColor = Color(0xFFFF6E5E);
+  final textColor = Colors.white;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: new BoxDecoration(color: Colors.yellowAccent),
+    return AspectRatio(
+      aspectRatio: containerWidth / containerHeight,
+      child: Container(
+        child: Stack(
+          children: <Widget>[
+            Align(
+              alignment: Alignment.centerRight,
+              child: AspectRatio(
+                aspectRatio: polygonWidth / polygonHeight,
+                child: SvgPicture.asset(
+                  "assets/svgs/logo_polygon.svg",
+                  color: polygonColor,
+                ),
+              ),
+            ),
+            Align(
+              // (W/2 - w/2) * x = centerX, (H/2 - h/2) * y = centerY
+              alignment: Alignment(
+                  -1.00,
+                  (titleY + (titleHeight/2) - (containerHeight/2)) / ((containerHeight/2) - (titleHeight/2))
+              ),
+              child: FractionallySizedBox(
+                widthFactor: titleWidth / containerWidth,
+                heightFactor: titleHeight / containerHeight,
+                child: AspectRatio(
+                  aspectRatio: titleWidth / titleHeight,
+                  child: SvgPicture.asset(
+                    "assets/svgs/logo_title.svg",
+                    color: textColor,
+                  ),
+                ),
+              ),
+            ),
+            Align(
+              // (W/2 - w/2) * x = centerX, (H/2 - h/2) * y = centerY
+              alignment: Alignment(
+                  (kX + (kWidth/2) - (containerWidth/2)) / ((containerWidth/2) - (kWidth/2)),
+                  (kY + (kHeight/2) - (containerHeight/2)) / ((containerHeight/2) - (kHeight/2))
+              ),
+              child: FractionallySizedBox(
+                widthFactor: kWidth / containerWidth,
+                heightFactor: kHeight / containerHeight,
+                child: AspectRatio(
+                  aspectRatio: kWidth / kHeight,
+                  child: SvgPicture.asset(
+                    "assets/svgs/logo_k.svg",
+                    color: textColor,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
